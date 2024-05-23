78a677ef9d96d47cdf9513136b0e5e83c022f007
Angel <angel.peraza@peaku.io>
2024-05-22 19:52:47 -0500

    Añadiendo requirements

--- models/empresa_model.py
+++ models/empresa_model.py
@@ -6,7 +6,7 @@ from GestionDePagosBackend.enum.tipo_cuenta_enum import Tipo_Cuenta
 class Empresa(Base):
     __tablename__ = "empresa"
     id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True )
-    nit = Column(Integer, primary_key=True, nullable=False)
+    nit = Column(Integer, primary_key=True, nullable=False, unique=True)
     nombre_empresa = Column(String, nullable=False)
     tipo_cuenta1 = Column(Enum(Tipo_Cuenta), nullable=False)
     nombre_banco1 = Column(String, nullable=False)
